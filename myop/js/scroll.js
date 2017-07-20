2bdc068c6af71d993b463749e23a9d0bcfd962ad
shunkai <shunkaiz@usc.edu>
2017-07-20 23:52:01 +0800

    clear

--- myop/js/scroll.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$(".page-scroll").click(function(event){
-		var anchor = $(this);
-		$("body").stop().animate({
-			scrollTop : ($($(this).attr("href")).offset().top-60)
-		}, 1250, 'easeInOutSine');
-		event.preventDefault();
-	});
-});
