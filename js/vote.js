10fe765c2a05e0c4dc970354387443f50bc6f18e
shunkai <shunkaiz@usc.edu>
2017-08-29 13:36:42 -0700

    vote button fix

--- js/vote.js
+++ js/vote.js
@@ -1,8 +1,10 @@
 $(function(){
 	$button = $(".caption").find("a");
+	//$button.attr('prop', 'Statistics');
 	$button.click(function(){
 		$candidate = $(this).parents(".caption").find("h3").text(); 
 		$user = $(".user-sm").text(); 
+		$(this).text("Statistics");
 		$.post('../myop/php/voting.php', {name: $candidate, user: $user}, function(data){
 			alert(data); 
 		});
